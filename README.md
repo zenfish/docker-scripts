43e44cebcac08762c53e94bbc19c6471a9478ae1
Dan Farmer <dan.farmer@multiscalehn.com>
2016-07-14 13:31:39 -0700

    first release... fly, little bird!

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-some docker scriptz
+docker scriptz
+--
+
+I've started to play with docker, so I thought I'd toss down a script or
+three as I do some things. Right now only one lonely one.
+
+consume-compose.py
+--
+
+A little program to take a docker compose file and output .dot
+format.  Requires the graphviz dot program to create image files.
+
+I tossed this together and then looked around; there are other things
+that might suit you better (https://github.com/funkwerk/compose_plantuml,
+https://github.com/Alexis-benoist/draw-compose, and probably others),
+but I wasn't getting what I personally wanted from their more
+advanced packages, so I thought I'd put this out there... just
+because, I guess.
+
+
+Mine is super simplistic and probably won't work on a compose file
+that uses advanced features or beyond my comprehension. But I've
+found it very helpful just to get a handle on what an app does when
+someone hands me a bunch of code.
+
+You can run it like:
+
+    # generate dot format - docker-compose.yml is simply a sample YAML file
+    consumer-compose.py docker-compose.yml > docker.dot
+    
+    # create a PNG using the output of the above
+    dot -Tpng -o docker.png < docker.dot
+
+    # or, force it to approximately some size desired with the Dsize arg.
+    dot -Gsize=6,6\! -Tpng -o compose.png < compose.dot
+
+And that'll create a PNG called docker.png (SVGs and other formats are
+possible as well, just change the dot command.)
+
+Given a compose file, the output looks like -
+
+![visualizing compose](https://github.com/zenfish/docker-scripts/raw/master/compose.png)
+
+Containers are circular, volumes are boxes, and networks are red
+double-lined circles ('cuz I've never actually seen one in action,
+but they're probably important ;))
+
+This requires the "dot" command to actually create any graphical
+output as well as the pygraphviz and docker compose libs 
+(e.g. "pip install pygraphviz docker-compose".)
+
+
---
+++ consume-compose.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+#
+# takes a compose file, hunts for connection statements, and tries 
+# to create dot vis output for graphing out the various lines of 
+# communication.
+#
+# Currently it looks for:
+#
+#   links
+#   external_links
+#   volumes
+#   ports
+#
+# it ignores lots of things that I don't understand or haven't used yet.
+#
+
+# requires pygraphviz & docker-compose python libs
+
+import sys
+import yaml
+import pygraphviz as pgv
+from collections import defaultdict
+# seemingly undocumented magic that might prove useful
+from compose.config.config import ConfigFile
+
+# arg sanity check
+try:
+    dcompose = ConfigFile.from_filename(sys.argv[1])
+
+except IndexError as e:
+    print "Usage: file.yml"
+    sys.exit(1)
+except Exception as e:
+    print "Error: %s" % e
+    sys.exit(2)
+
+filename      = sys.argv[1]
+# assume it's a foo.bar kinda file
+filebase      = filename.split('.')[0]
+
+# this is where we draw the relationships
+compose       = defaultdict(lambda : defaultdict(list))
+# various things to look for inside compose file
+compose_items = ["links", "external_links", "volumes", "ports"]
+
+# graph object
+dvis          = pgv.AGraph(strict=False,directed=True)
+
+# print ConfigFile.get_service_dicts(dcompose)
+
+# only know of 2 versions so far... assume it's one unless proven otherwise
+containers = dcompose.config
+
+try:
+    dcompose.config['services']
+    assert dcompose.config['version'] == '2'
+    containers = dcompose.config['services']
+    # print "Looks like V2!\n\n"
+
+except Exception as e:
+    pass
+
+# for container, config in dcompose.config.iteritems():
+for container, config in containers.iteritems():
+
+    # print config.keys()
+
+    # first add any nodes
+    dvis.add_node(container, style="filled", fillcolor="lightblue", fontsize="10")
+
+    ports = ''
+    # use this to add to links or other connectors if applicable
+    if 'ports' in config.keys():
+        ports = 'port(s):' + ','.join(config['ports'])
+
+    # treating links and external links the same
+    if 'links' in config.keys():
+        for link in config['links']:
+            link = link.split(':')[0]
+            if ports:
+                dvis.add_edge(container, link, label=ports, style="filled", fontsize="10", fillcolor="lightgray")
+            else:
+                dvis.add_edge(container, link, style="filled", fontsize="10", fillcolor="lightgray")
+
+    if 'external_links' in config.keys():
+        for link in config['external_links']:
+            dvis.add_edge(container, link, style="filled", fillcolor="lightgray")
+
+    # goes... to somewhere!
+    if 'networks' in config.keys():
+        for network in config['networks']:
+            network = network.split(':')[0]
+            dvis.add_node(network, style="filled", fillcolor="firebrick1", shape="doublecircle", fontsize="10")
+            dvis.add_edge(container, network)
+
+
+    # drives/files/whatever
+    if 'volumes' in config.keys():
+        for volume in config['volumes']:
+            dvis.add_node(volume, style="filled", fillcolor="lightgrey", shape="box", fontsize="8")
+#           print "V"
+            # print container, link
+            dvis.add_edge(container, volume, style="dashed")
+
+
+# a little header to remind me how to us this
+print """
+//
+// Create a PNG file from the output using:
+//
+//     dot -Tpng -o %s.png < %s.dot
+//
+
+""" % (filebase, filebase)
+
+print dvis.string()
+
+
+# print "\n".join('"{}" -> "{}"'.format(s, format_dest(d)) for s, d in iter_links(dcompose))
+
